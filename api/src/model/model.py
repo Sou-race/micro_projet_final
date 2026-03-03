a16bffabf95b61e9b84e00bcd4a4f2fc83704484
HappyGood <simon.carriac@gmail.com>
2026-03-03 12:01:35 +0100

    fit : debut resources et model

--- api/src/model/model.py
+++ api/src/model/model.py
@@ -1,1 +1,6 @@
+from pydantic import BaseModel, ConfigDict, Field
 
+class ModelResponseToFront(BaseModel):
+    name : str = Field(..., description="Le nom de la route recommandée")
+    accuracies : list[float] 
+    isFinished : bool
